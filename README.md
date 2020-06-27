b18a0160a0541dac0a29225dfe6d8a1c89dc246d
zhou-meijun <617889457@qq.com>
2020-06-27 14:27:18 +0800

    优化：DllPlugin

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # webpackDemo-study
+
+# Clone project
+git clone https://github.com/MeiJun-95/webpackDemo-study.git
+
+# Install dependencies
+npm install
+
+# Compiles and hot-reloads for development
+npm run dev
+
+# Compiles and minifies for production
+npm run build
+
+
+
+
+DLLPlugin 动态链接库插件
+前端框架 vue React，体积大，构建慢
+较稳定，不常升级版本
+同一版本只构建一次即可，不用每次都重新构建
+
+怎么使用：
+ 1. webpack已内置 DllPlugin 支持
+ 2. DllPlugin - 打包出 dll 文件 ： 先用DllPlugin将vue/React打包出一个文件 （中间产物：一个内容，一个索引）
+ 3. DllReferencePlugin - 使用 dll 文件： 再引用时直接使用这个打包结果就可以，不需要重新打包（只要没升级）
+ 4. 在html中 设置引用方式：<script src="./react.dll.js"></script>。 
+    在webpack中 使用 DllReferencePlugin 设置引用方式（步骤3）
+    js中引用方式就不用修改了
+    
--- webpack-dll-demo/build/webpack.dll.js
+++ webpack-dll-demo/build/webpack.dll.js
@@ -3,21 +3,22 @@ const DllPlugin = require('webpack/lib/DllPlugin')
 const { srcPath, distPath } = require('./paths')
 
 module.exports = {
-  mode: 'development',
+  mode: 'development',  //一般开发环境使用，线上不必要
   // JS 执行入口文件
   entry: {
+    // 这个文件的目的是：预先打包react、react-dom，打包第三方插件，所以这里入口文件名这样设置
     // 把 React 相关模块的放到一个单独的动态链接库
     react: ['react', 'react-dom']
   },
   output: {
     // 输出的动态链接库的文件名称，[name] 代表当前动态链接库的名称，
     // 也就是 entry 中配置的 react 和 polyfill
-    filename: '[name].dll.js',
+    filename: '[name].dll.js',   //存放 ['react', 'react-dom']所有的文件
     // 输出的文件都放到 dist 目录下
     path: distPath,
     // 存放动态链接库的全局变量名称，例如对应 react 来说就是 _dll_react
     // 之所以在前面加上 _dll_ 是为了防止全局变量冲突
-    library: '_dll_[name]',
+    library: '_dll_[name]',    //输出的是一个第三方的库，就要配置一个library，这是一个全局变量名
   },
   plugins: [
     // 接入 DllPlugin
@@ -27,7 +28,7 @@ module.exports = {
       // 例如 react.manifest.json 中就有 "name": "_dll_react"
       name: '_dll_[name]',
       // 描述动态链接库的 manifest.json 文件输出时的文件名称
-      path: path.join(distPath, '[name].manifest.json'),
+      path: path.join(distPath, '[name].manifest.json'), //就是一个对.dll.js文件的索引
     }),
   ],
 }
